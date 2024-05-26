f3f6bc65ff25cc9a092892c75d1f625e5f6ea919
lwilsen <120128777+lwilsen@users.noreply.github.com>
2024-05-26 18:53:40 -0400

    Chapter 11 and 12 ML SKlearn + Pytorch notes

---
+++ Titanic/stochastic_MC_PI.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def estimate_pi(num_samples):
+    count_inside_circle = 0
+    for _ in range(num_samples):
+        x, y = np.random.rand(2)
+        if x**2 + y**2 <= 1:
+            count_inside_circle += 1
+    return (4 * count_inside_circle) / num_samples
+
+pi_estimate = estimate_pi(1000000)
+print(f"Estimated value of π: {pi_estimate}")
