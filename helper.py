709320d756e86f561d1fe9ab73a06901f0f1434e
lwilsen <120128777+lwilsen@users.noreply.github.com>
2024-05-29 19:49:54 -0400

    Neural Networks + helper py file

---
+++ helper.py
@@ -0,0 +1,34 @@
+# NN trainer
+import torch
+import torch.nn as nn
+
+# need to define a model, num epochs, data loader, and batch size first
+
+loss_fn = nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.015)
+    
+torch.manual_seed(1)
+
+loss_hist_train = [0] * num_epochs
+accuracy_hist_train = [0] * num_epochs
+loss_hist_valid = [0] * num_epochs
+accuracy_hist_valid = [0] * num_epochs
+for epoch in range(num_epochs):
+    for x_batch, y_batch in train_dl:
+        pred = model(x_batch, True)[:, 0]
+        loss = loss_fn(pred, y_batch)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        loss_hist_train[epoch] += loss.item()
+        is_correct = ((pred>=0.5).float() == y_batch).float()
+        accuracy_hist_train[epoch] += is_correct.mean()
+
+    loss_hist_train[epoch] /= n_train/batch_size
+    accuracy_hist_train[epoch] /= n_train/batch_size
+
+    pred = model(x_valid)[:, 0]
+    loss = loss_fn(pred, y_valid)
+    loss_hist_valid[epoch] = loss.item()
+    is_correct = ((pred>=0.5).float() == y_valid).float()
+    accuracy_hist_valid[epoch] += is_correct.mean()
