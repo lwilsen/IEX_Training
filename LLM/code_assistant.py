cba3eb0dd835ccd0f9bc34dd3814674ef328f8a3
lwilsen <120128777+lwilsen@users.noreply.github.com>
2024-06-17 14:18:50 -0400

    Cohere chatbot local and streamlit

---
+++ LLM/code_assistant.py
@@ -0,0 +1,14 @@
+import cohere
+import os
+
+cohere_api_key = os.getenv("CO_API_KEY")
+co = cohere.Client(api_key=cohere_api_key)
+
+message = """Can you fix the errors in the following python code: print('luke Wilsen)"""
+
+response = co.chat(
+    model = "command-r-plus",
+    message= message
+)
+
+print(response.text)
