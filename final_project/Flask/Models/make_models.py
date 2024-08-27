c1d240f6d612ac1de279ba3be99e1a16e4e1c8c7
lwilsen <120128777+lwilsen@users.noreply.github.com>
2024-08-27 14:55:53 -0400

    fixed some problems to test sonarcloud

--- final_project/Flask/Models/make_models.py
+++ final_project/Flask/Models/make_models.py
@@ -5,15 +5,17 @@ import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from nltk.stem.porter import PorterStemmer
+from joblib import Memory
 
+memory = Memory(location='./cache')
 
 #Housing regression model creation
 from final_project.Flask.utils import LogTransformer
     
 ridge_pipeline = Pipeline([
     ('log_transformer', LogTransformer(add_constant=1)),
-    ('ridge_regression', Ridge())
-])
+    ('ridge_regression', Ridge(random_state=42))
+], memory = memory)
 
 with open('ridge_pipe.pkl', 'wb') as f:
     pickle.dump(ridge_pipeline, f)
@@ -30,7 +32,7 @@ tfidf = TfidfVectorizer(strip_accents=None,
                         preprocessor=None)
 
 lr_tfidf = Pipeline([('vect', tfidf),
-                     ('clf', LogisticRegression(solver='liblinear')),])
+                     ('clf', LogisticRegression(solver='liblinear', random_state=42)),], memory=memory)
 
 best_params = {'clf__C': 10.0, 'clf__penalty': 'l2', 'vect__ngram_range': (1, 1), 'vect__stop_words': None, 'vect__tokenizer': tokenizer_porter}
 lr_tfidf.set_params(**best_params)
