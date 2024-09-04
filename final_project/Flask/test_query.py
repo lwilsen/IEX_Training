e93b698593dded21f5b77d4c6b072db805e45094
lwilsen <lwilsen@umass.edu>
2024-09-04 11:19:24 -0400

    Updating Final Project

--- final_project/Flask/test_query.py
+++ final_project/Flask/test_query.py
@@ -1,12 +1,10 @@
 import requests
-import json
 
 url = "http://localhost:8000/query"
 
-query = '''SELECT * FROM titanic LIMIT 5;'''
+query = """SELECT * FROM titanic LIMIT 5;"""
 
-#j_query = json.dumps(query)
 
-response = requests.post(url, json = {'query':query})
+response = requests.post(url, json={"query": query}, timeout=10)
 
 print(response, response.text)
--- final_project/streamlit/pages/1_Data.py
+++ final_project/streamlit/pages/1_Data.py
@@ -1,13 +1,15 @@
-import streamlit as st
+import re
+
 import requests
 import pandas as pd
-import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import nltk
 
-nltk.download('punkt_tab')
-nltk.download('stopwords')
+import streamlit as st
+
+nltk.download("punkt_tab")
+nltk.download("stopwords")
 st.title("Data Exploration")
 
 if st.sidebar.button("Reset Page"):
@@ -17,25 +19,30 @@ if st.sidebar.button("Reset Page"):
         document.documentElement.scrollTop = 0;
         </script>
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
 
 
 def housing(df):
-    st.write('---')
-    st.write("""
+    st.write("---")
+    st.write(
+        """
     # Housing Price Linear Regression
 
     This data came from the assessor's office of Ames Iowa, and contains information used to computing 
             assessed values for individual residential properties sold in ames, IA from 2006 to 2010.
-        """)     
+        """
+    )
     st.write("""---""")
-    st.image("/app/pictures/Ames_Iowa.jpg",caption="Ames Iowa", use_column_width=True)
-    st.write("""        
+    st.image("/app/pictures/Ames_Iowa.jpg", caption="Ames Iowa", use_column_width=True)
+    st.write(
+        """        
     I chose to look at a subsection of the entire dataset, selecting only the following columns:
-    """)
+    """
+    )
 
-    st.write("""
+    st.write(
+        """
                 
         | Column Name    | Description                                                                     |
         |----------------|---------------------------------------------------------------------------------|
@@ -48,42 +55,63 @@ def housing(df):
         | Fireplaces     | The number of fireplaces. (discrete)                                            |
         | Exter Qual     | Evaluates the quality of the material on the exterior.                          |
         ---
-        """)
-    
+        """
+    )
+
     st.subheader("""Queried Data""")
     st.dataframe(df)
     st.divider()
-    st.write("When I was exploring the data, I found that our outcome variable, *saleprice* was very skewed. This isn't good because a linear model technically requires non-skewed data to work properly!")
-    st.image("/app/pictures/Housing_Normalization.png", caption="Normalization", use_column_width=True)
-    st.write("""
+    st.write(
+        "When I was exploring the data, I found that our outcome variable, *saleprice* was very skewed. This isn't good because a linear model requires non-skewed data to work properly!"
+    )
+    st.image(
+        "/app/pictures/Housing_Normalization.png",
+        caption="Normalization",
+        use_column_width=True,
+    )
+    st.write(
+        """
 - You can see below that when the data isn't normalized the errors aren't **random**
-    - This means your predictions are going to be wrong in a predictable way, and we can find and eliminate that pattern of error""")
+    - This means your predictions are going to be wrong in a predictable way, and we can find and eliminate that pattern of error"""
+    )
     st.image("/app/pictures/Non_Normal_Residuals.png", use_column_width=True)
-    st.write('''
+    st.write(
+        """
 - However, after normalization (below), the errors are more random (I promise this is a good thing)
-    - It means that we've extracted all of the patterns (predictability) out of the data we can!''')
+    - It means that we've extracted all of the patterns (predictability) out of the data we can!"""
+    )
     st.image("/app/pictures/Normalized_residuals.png", use_column_width=True)
     st.divider()
-    st.write("""I also tried to change different parameters of my best model, to see if I could fine tune it further, with limited success""")
+    st.write(
+        """I also tried to change different parameters of my best model, to see if I could fine tune it further, with limited success"""
+    )
     st.image("/app/pictures/LR_val_curve.png", use_column_width=True)
-    st.write("""
-- You can see a slight slope in both lines (indicating better performance), but not by a practically significant amount""")
+    st.write(
+        """
+- You can see a slight slope in both lines (indicating better performance), but not by a practically significant amount"""
+    )
     st.divider()
     st.subheader("Conclusions")
     st.image("/app/pictures/LR_coefficient_estimates.png", use_column_width=True)
-    st.write("""
-    - Because the model used data that was log transformed, the table above contains all the coefficients raised to the power e
-    - We can see that the most important predictors are:
-        - Central Air
-        - Exter Qual
-        - Overall Qual
-    - Here, an **increase by 1** in any of the parameters causes an **increase by one hundred thousand dollars** in saleprice.
-        - Ex. According to the model, an increase in central air corresponds to an increase in price by 0.243 hundred thousand dollars ($24,300).
-    """)
-    return
+    st.write(
+        """
+**Important predictors**
+  
+- Central Air
+- Exter Qual
+- Overall Qual
+    - To see how a single parameter impacts price, **multiply the parameter by $100,00**
+        - Ex. According to the model, **having central air** corresponds to an **increase in price by 0.243 hundred thousand dollars ($24,300)**.
+    """
+    )
 
-def titanic(df):
-    st.write("""
+
+def titanic(df1):
+
+    st.subheader("Queried Data")
+    st.dataframe(df1)
+    st.write(
+        """
     ---       
     ## Columns and what they represent
             
@@ -96,36 +124,46 @@ def titanic(df):
     | Age            | The passenger's age in years. Fractional values may exist for younger children. |
     | SibSp          | The number of siblings or spouses traveling with the passenger.                   |
     | Parch          | The number of parents or children traveling with the passenger.                   |
-    | Fare           | The price the passenger paid for their ticket.                                  |
-    | class_1 - class_3          | The Passenger's room class.                                    |   
-    """)
-    st.divider()
-    st.subheader("Queried Data")
-    st.dataframe(df)
+    | Fare           | The price the passenger paid for their ticket.                                  || class_1 - class_3          | The Passenger's room class.                                    |   
+    """
+    )
     st.divider()
     st.subheader("Data Exploration")
-    st.write("I wanted to visually explore the relationship between the different variables, so I used a heatmap")
+    st.write(
+        "I wanted to visually explore the relationship between the different variables, so I used a heatmap"
+    )
     st.image("/app/pictures/Titanic_heatmap.png", use_column_width=True)
-    st.write("- From the heatmap *survived* row, you can see that being **male** had a negative impact on survival, and that the **fare** you paid and the **class** you were in had either a negative or positive impact")
-    st.write("""I also divided the passengers into male and female to look at the differences in survival""")
+    st.write(
+        "- From the heatmap *survived* row, you can see that being **male** had a negative impact on survival, and that the **fare** you paid and the **class** you were in had either a negative or positive impact"
+    )
+    st.write(
+        """I also divided the passengers into male and female to look at the differences in survival"""
+    )
     st.image("/app/pictures/Titanic_pass_breakdown_sex.png", use_column_width=True)
-    st.write("""
+    st.write(
+        """
 - You can see from the above graphs, that though **most of the passengers were men, most of the survivors were female**
     - Therefore it's likely that being male negatively impacted your chances of survival
              
 I also wanted to look at the fare distrubution seperated by survival status
-""")
+"""
+    )
     st.image("/app/pictures/Titanic_violin.png", use_column_width=True)
-    st.write("""
+    st.write(
+        """
 - The mean (dotted) lines in the above plot show us that the average *fare* paid was higher for those who survived,
              than for those that died
     - Therefore, it's also likely that the **more you paid, the better your chances of survival** were
         - Class could also have paid a role too, because if you paid more, you were more likely to be in first class, 
              which was higher up in the ship, also making survival more likely
-""")
-    st.write("I also tried a non-linear clustering method to see if there were two distinct clusters in the data")
+"""
+    )
+    st.write(
+        "I also tried a non-linear clustering method to see if there were two distinct clusters in the data"
+    )
     st.image("/app/pictures/Titanic_DBSCAN.png", use_column_width=True)
-    st.write("""
+    st.write(
+        """
 - Visually, you can see that there are two majority groups and one very small minority group
     - My hypothesis about this is that the two majority groups are:
              
@@ -135,111 +173,154 @@ I also wanted to look at the fare distrubution seperated by survival status
                 
     **3.** Survived, rich, 1st class and most likely male
     - This is just based on what I was seeing as the biggest clusters, and the evidence from the violin plot above
-""")
+"""
+    )
     st.divider()
     st.subheader("Conclusions")
-    st.write("""
+    st.write(
+        """
 Overall the insights that I gained from this analysis were:
 - Being male, 
 - Being poor 
 - And being in 3rd or 2nd class 
              
 **Negatively** impacted odds of survival. These conclusions back up most of what we already knew about the sinking of the Titanic.
-""")
-    return
+"""
+    )
+
 
 def preprocessor(text):
-    text = re.sub('<[^>]*>',"",text)
-    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
-    text = (re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', ''))
-    
+    text = re.sub("<[^>]*>", "", text)
+    emoticons = re.findall(r"(?::|;|=)(?:-)?(?:\)|\(|D|P)", text)
+    text = re.sub(r"[\W]+", " ", text.lower()) + " ".join(emoticons).replace("-", "")
+
     return text
 
-def movies(df):
-    st.write('**IMDB Dataframe**')
-    st.dataframe(df)
+
+def movies(df2):
+    st.write("**IMDB Dataframe**")
+    st.dataframe(df2)
     st.divider()
-    movie_1 = (df.iloc[1,0])
-    st.text_area("Unprocessed Review",movie_1,height=200)
+    movie_1 = df2.iloc[1, 0]
+    st.text_area("Unprocessed Review", movie_1, height=200)
     processed_review = preprocessor(movie_1)
-    st.text_area("Cleaned Review", processed_review, height = 200)
+    st.text_area("Cleaned Review", processed_review, height=200)
     tokenized_review = word_tokenize(processed_review)
-    st.text_area("Tokenized Review", tokenized_review, height = 250)
+    st.text_area("Tokenized Review", tokenized_review, height=250)
     stop = stopwords.words("english")
-    st.text_area("Processed Review", [word for word in tokenized_review if word not in stop], height=200)
+    st.text_area(
+        "Processed Review",
+        [word for word in tokenized_review if word not in stop],
+        height=200,
+    )
     st.divider()
-    st.write('**Models**')
-    st.write("""
+    st.write("**Models**")
+    st.write(
+        """
 I used two models:
 1. Logistic Regression Classifier
     - This was the better performer
 2. Out of Core Learning with a Stochastic Gradient Descent Classifier
     - This model iterates through "batches" of the total dataset, and updates itself during the process
         - This method is better for larger datasets and more complicated patterns
-""")
+"""
+    )
     st.divider()
     st.write("**Confusion Matrix**")
-    st.write("""
+    st.write(
+        """
 - Confusion Matrices are often used for visually evaluating sentiment analysis
 - You can see that my best model was better at predicting positive sentiments than negative sentiments
     - There are more incorrect "True Negative" reviews, than incorrect "True Positive" reviews
-""")
-    st.image("/app/pictures/NLP_Confusion.png",caption="Confusion Matrix of model predictions", use_column_width=True)
-    return
+"""
+    )
+    st.image(
+        "/app/pictures/NLP_Confusion.png",
+        caption="Confusion Matrix of model predictions",
+        use_column_width=True,
+    )
+    st.divider()
+    st.subheader("Take-away")
+    st.write(
+        """
+- There aren't really any *insights* to take away from this dataset, **but**, we do now have a cool model that can analyze the sentiment of any text passed to it!
+"""
+    )
+
 
 def mnist():
-    st.write('**Examples of MNIST Handwritten Digits**')
-    st.image("/app/pictures/MNIST_digits.png", caption="MNIST DIGITS", use_column_width=True)
+    st.write("**Examples of MNIST Handwritten Digits**")
+    st.image(
+        "/app/pictures/MNIST_digits.png", caption="MNIST DIGITS", use_column_width=True
+    )
     st.header("Convolutional Neural Network Using *Tensorflow*")
-    st.markdown("""
+    st.markdown(
+        """
 I used a Convolutional Neural Network (CNN) to classify the images contained in the MNIST dataset, using both Pytorch and Tensorflow. Here is a brief explanation of each layer in the CNN model and its role in the classification process:
-""")
+"""
+    )
 
     st.subheader("Neural Network Architecture Explained")
 
-    st.write("""
+    st.write(
+        """
 ### 1. Flatten Layer
 - **What It Does**: This layer takes the image, which is originally 28 by 28 pixels, and stretches it out into a single line with 784 values. This step is needed so that the image data can be processed by the next layers.
-""")
+"""
+    )
 
-    st.write("""
+    st.write(
+        """
 ### 2. Decision-Making Layer 1 (Dense Layer 1)
 - **What It Does**: This layer has 128 decision-making units (neurons). These units learn to detect important features from the stretched-out image data. They use a method called "ReLU" that helps the network identify complex patterns and relationships within the data.
-""")
+"""
+    )
 
-    st.write("""
+    st.write(
+        """
 ### 3. Final Decision (Output Layer)
 - **What It Does**: The final layer has 10 units, one for each possible digit (0-9). This layer makes the final decision by processing the data into 10 possible outcomes (one for each digit). Although this layer uses a "linear" method at first, a step called "softmax" is applied later to convert the results into probabilities, which tells us how confident the model is in each choice.
-""")
+"""
+    )
     st.subheader("Model Compilation")
-    st.write("""
+    st.write(
+        """
 ### 1. Optimizer: Adam
 - **What It Does**: The Adam optimizer helps the model learn from the training data by adjusting the model's internal settings (weights) based on the data it sees. It combines the benefits of two other methods, which makes learning faster and more efficient.
-""")
+"""
+    )
 
-    st.write("""
+    st.write(
+        """
 ### 2. Loss Function: Sparse Categorical Crossentropy
-- **What It Does**: This function is like a guide that helps the model learn to make better predictions. It compares the model’s guesses with the correct answers (the target labels). The `from_logits=True` part simply means that the model's raw outputs (logits) are automatically turned into probabilities before this function does its job.
-""")
+- **What It Does**: This function is like a guide that helps the model learn to make better predictions. It compares the model’s guesses with the correct answers (the target labels), tells the model how far off it is, and in which direction to go to get better.
+"""
+    )
 
-    st.write("""
+    st.write(
+        """
 ### 3. Evaluation Metric: Accuracy
-- **What It Does**: Accuracy tells us how well the model is performing by calculating the percentage of correctly guessed images. If the model's predictions match the correct labels, it gets points for accuracy, and we use this to judge how well the model is doing overall.
-""")
+- **What It Does**: Accuracy tells us how well the model is performing by calculating the percentage of correctly guessed images. This metric tells us how well the model did overall, and at each step through its learning process.
+"""
+    )
+
+    st.write("---")
+    st.write("Model Training and Validation")
+    st.image(
+        "/app/pictures/Screenshot 2024-05-29 at 7.53.46 PM.png",
+        caption="Train and Validation Accuracy through Epoch Progression",
+    )
+
+    st.write("Graphs of model performance through epoch progression")
+    st.image(
+        "/app/pictures/Screenshot 2024-05-29 at 7.54.14 PM.png",
+        caption="Model Performance",
+    )
 
 
-    st.write('---')
-    st.write('Model Training and Validation')
-    st.image('/app/pictures/Screenshot 2024-05-29 at 7.53.46 PM.png', caption="Train and Validation Accuracy through Epoch Progression")
-
-    st.write('Graphs of model performance through epoch progression')
-    st.image('/app/pictures/Screenshot 2024-05-29 at 7.54.14 PM.png',caption='Model Performance')
-
-    return
-
-data_option = st.radio("Select Data:", 
-                        ("Housing","Titanic", "Movie", "MNIST"),
-                        key = "data_option")
+data_option = st.radio(
+    "Select Data:", ("Housing", "Titanic", "Movie", "MNIST"), key="data_option"
+)
 
 query = ""
 if data_option == "Titanic":
@@ -251,23 +332,25 @@ elif data_option == "Movie":
 elif data_option == "MNIST":
     query = "-- MNIST is non-queriable from the SQL database"
 
-query = st.text_area(label = '**Enter your SQL query here:**', value = query)
+query = st.text_area(label="**Enter your SQL query here:**", value=query)
 
 if st.button("Submit"):
-    response = requests.post("http://flask_route:5001/query", json = {"query":query})
+    response = requests.post(
+        "http://flask_route:5001/query", json={"query": query}, timeout=10
+    )
     if response.status_code == 200:
         try:
             result = response.json()
-            
+
             data = result.get("Data", [])
             columns = result.get("Columns", [])
-            df = pd.DataFrame(data, columns=columns)
+            dataframe = pd.DataFrame(data, columns=columns)
             if data_option == "Housing":
-                housing(df)
+                housing(dataframe)
             elif data_option == "Titanic":
-                titanic(df)
-            elif data_option =="Movie":
-                movies(df)
+                titanic(dataframe)
+            elif data_option == "Movie":
+                movies(dataframe)
 
         except requests.exceptions.JSONDecodeError:
             st.error("Error: The response is not in JSON format.")
