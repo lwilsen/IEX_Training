0b4548e54243d14f5101180d629aa4ebcf396ddb
lwilsen <lwilsen@umass.edu>
2025-03-08 15:25:20 -0500

    Updated sentiment analysis app to reflect correct purpose

--- LLM/sentiment_app.py
+++ LLM/sentiment_app.py
@@ -4,8 +4,8 @@ import os
 
 
 def main():
-    st.title("Python Code Assistant")
-    st.write("Python Code Helper! Enter your problematic code!")
+    st.title("Text Sentiment Analysis")
+    st.write("Enter a paragraph and we will analyze the sentiment of the text.")
 
     text_input = st.text_area("Enter Paragraph to Analyze:", height=400)
     if st.button("Analyze Code"):
