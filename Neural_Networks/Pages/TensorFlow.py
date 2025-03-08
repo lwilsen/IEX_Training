4e68dd00cca3c845a89eac0fbb97f81a18f69440
lwilsen <lwilsen@umass.edu>
2025-03-08 17:05:03 -0500

    Finished NN app, and repo org

--- Neural_Networks/Pages/TensorFlow.py
+++ Neural_Networks/Pages/TensorFlow.py
@@ -30,8 +30,28 @@ In our Streamlit app, we use a simple Neural Network (NN) to classify the images
     - This loss function is used for classification tasks where the target labels are integers. The `from_logits=True` argument indicates that the output values are raw logits, which will be converted to probabilities internally.
 - **Metrics**: Accuracy
     - The accuracy metric is used to evaluate the performance of the model by calculating the proportion of correctly classified images.
-
-### Summary:
-The NN model processes the MNIST images through a series of dense layers. It first flattens the input images, then uses a hidden dense layer to learn patterns, and finally outputs logits for each of the 10 classes. The model learns to recognize patterns and structures in the images, enabling it to accurately classify handwritten digits.
 """
 )
+
+
+st.write("---")
+st.write("Model Training and Validation")
+st.image(
+    "/Users/lukewilsen/Desktop/IEX/IEX_Training/Neural_Networks/screenshots/Tensor_Flow_sc.png",
+    caption="Train and Validation Accuracy through Epoch Progression",
+)
+
+st.write("Graphs of model accuracy through epoch progression")
+st.image(
+    "/Users/lukewilsen/Desktop/IEX/IEX_Training/Neural_Networks/screenshots/TF_accuracy.png",
+    caption="Model Performance",
+)
+st.image(
+    "/Users/lukewilsen/Desktop/IEX/IEX_Training/Neural_Networks/screenshots/TF_mod_loss.png",
+    caption="Model Loss",
+)
+
+st.write("""
+### Summary:
+The NN model processes the MNIST images through a series of dense layers. It first flattens the input images, then uses a hidden dense layer to learn patterns, and finally outputs logits for each of the 10 classes. The model learns to recognize patterns and structures in the images, enabling it to accurately classify handwritten digits.
+""")
