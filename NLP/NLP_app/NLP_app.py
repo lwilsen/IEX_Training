a773ad9f144b1ac1d9cc27a491382e471363b589
lwilsen <lwilsen@umass.edu>
2025-03-08 16:28:39 -0500

    NLP repo app re-org

---
+++ NLP/NLP_app/NLP_app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+from PIL import Image
+
+# Set the title and introduction
+st.title("Natural Language Processing with IMDB Movie Reviews")
+st.markdown(
+    """
+Welcome to the **Natural Language Processing (NLP) Demo App**! This app demonstrates the basics of NLP using the classic IMDB movie review dataset. You'll learn about text preprocessing, sentiment analysis, and more.
+
+The IMDB dataset contains 50,000 movie reviews labeled as positive or negative, making it an excellent resource for binary sentiment classification tasks.
+
+---
+
+## Outline
+- **Text Preprocessing**: Cleaning and preparing text data for analysis.
+- **Vectorization**: Converting text data into numerical representations.
+- **Sentiment Analysis**: Classifying text as positive or negative.
+- **Model Evaluation**: Assessing the performance of your model.
+
+---
+
+## How to Use This App
+1. **Navigate** through the sidebar to explore different sections.
+
+Let's get started and dive into the fascinating world of NLP!
+"""
+)
+
+st.write("---")
+# Main page content
+st.header("Introduction")
+st.markdown(
+    """
+Natural Language Processing (NLP) is a subfield of artificial intelligence that focuses on the interaction between computers and humans through natural language. In this demo, we will use the IMDB movie review dataset to perform basic NLP tasks.
+"""
+)
---
+++ NLP/NLP_app/pages/3_Training_and_Evaluation.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+st.header("Model Training and Evaluation")
+st.write("---")
+st.write("## Logistic Regression")
+st.write(
+    """The first model we're going to use for our NLP is a logistic Regression Classifier, 
+         that is based on a bag of words model, that represents how common each stemmed word is in our dataset.
+         The parameters for this model are as follows: 
+         
+         'clf__C': 10.0, 'clf__penalty': 'l2', 'vect__ngram_range': (1, 1), 'vect__stop_words': None, 'vect__tokenizer': tokenizer_porter"""
+)
+st.write(
+    """**The accuracy for this model was 89.2% on the training data, and 89.3% on the testing dataset.**
+"""
+)
+st.write("---")
+st.write("## Out-of-Core Learning with a Stochastic Gradient Descent Classifier")
+st.write(
+    """The previous model took a very long time to run, mostly due to the grid search performed, and 
+         one solution to this is to incrementally fit the classifier to smaller batches of the dataset. This method
+         uses stochastic gradient descent to incrementally optimize the loss function we choose (log loss in this case).
+         **The accuracy for this model was 86.6% on the test data.**"""
+)
+
+df = pd.read_csv("movie_data.csv", encoding="utf-8")
